ce03869f068c1fd71d17575ac12d1843fb4d13e3
CyberByte Academy <shahrianapurbo@gmail.com>
2023-08-25 16:41:03 +0300

    ‎

---
+++ logic/index.js
@@ -0,0 +1,8 @@
+let mobileMenuOpen = false;
+
+function shiftMenu() {
+    mobileMenuOpen === true ?
+        document.querySelector('.mobile-nav').style.height = '0px' :
+        document.querySelector('.mobile-nav').style.height = '100px';
+    mobileMenuOpen = !mobileMenuOpen;
+}
